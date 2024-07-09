5ac0f6656243b79ad47e614b23dfe2d9827e815f
lotem <elazarlotem1808@gmail.com>
2024-07-09 16:43:58 +0300

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 function App() {
   const [messagesQueue, setMessagesQueue] = useState([]);
   const [currentMessage, setCurrentMessage] = useState("No messages yet...");
   const [countdown, setCountdown] = useState(null);
+  const [darkMode, setDarkMode] = useState(true); // Set dark mode as default
   const ws = useRef(null);
 
   useEffect(() => {
-    ws.current = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
-
+    // ws.current = new WebSocket(process.env.REACT_APP_WEBSOCKET_TEST_URL);
+    ws.current = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL); // Use custom environment variable
     ws.current.onopen = () => {
       console.log("WebSocket connection established");
     };
@@ -54,10 +56,25 @@ function App() {
     }
   }, [messagesQueue, countdown]);
 
+  const isHebrew = (text) => {
+    return /[\u0590-\u05FF]/.test(text);
+  };
+
+  useEffect(() => {
+    document.body.className = darkMode ? "dark-mode" : "light-mode";
+  }, [darkMode]);
+
   return (
-    <div className="App">
+    <div className={`App`}>
+      <div className="toggle-container">
+        <DarkModeSwitch checked={darkMode} onChange={setDarkMode} size={30} />
+      </div>
       <div id="message-container">
-        <div id="message">{currentMessage}</div>
+        <div
+          id="message"
+          style={{ direction: isHebrew(currentMessage) ? "rtl" : "ltr" }}>
+          {currentMessage}
+        </div>
         {countdown !== null && (
           <div id="countdown">New message in {countdown}</div>
         )}
